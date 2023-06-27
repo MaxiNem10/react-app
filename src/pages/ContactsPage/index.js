d228083a7260b5288b257e97c343078c3bb7570c
Maxinem10 <maxinem190790@gmail.com>
2023-06-28 07:17:03 +0800

    router fix

---
+++ src/pages/ContactsPage/index.js
@@ -0,0 +1,10 @@
+const ContactsPage = () => {
+    return (
+      <div>
+        Наш номер телефона:
+        <a href="tel:+79516206273">+79516206273</a>
+      </div>
+    );
+  };
+  
+  export default ContactsPage;
