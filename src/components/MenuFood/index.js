4e843e7f1890d0a7269eda81164c95be4bdc49e7
Maxinem10 <maxinem190790@gmail.com>
2023-07-02 19:46:25 +0800

    catalog finish

--- src/components/MenuFood/index.js
+++ src/components/MenuFood/index.js
@@ -1,54 +1,10 @@
 
+import menu from "../FakeData/menu";
 import MenuCard from "./MenuCard";
 
 
 const Menu = ({onItemBuy}) => {
-  let menu = [
-    {
-      title: "Суп Шулэн",
-      sostav: ["Лапша", "мясо", "картошка"],
-      priceCooking: 0,
-      price: 200, // 'стоимость Шулэна 200 рублей' ,
-      image: "/assets/shulen.jpg",
-    },
-    {
-      title: "Буузы",
-      sostav: ["Тесто", "фаршговяжий", "фаршсвиной", "лук"],
-      priceCooking: 0,
-      price: 250, //'стоимость бууз: 40 руб за 1 шт., 35 руб за 1 шт. замороженные',
-      image: "https://voyagerestaurant.ru/wp-content/uploads/2020/07/hot_14.jpg",  
-    },
-    {
-      title: "Цуйван",
-      sostav: ["Лапша", "мясо", "перецболгарский", "лук"],
-      priceCooking: 0,
-      price: 150, //'стоимость Цуйвана 150 руб за 200 гр., стоимость Цуйвана 250 руб за 400 гр.',
-      image: "https://voyagerestaurant.ru/wp-content/uploads/2020/07/udon-grudka.jpg",
-    },
-   {
-    title:'Борщ',
-    sostav: ['Капуста', 'мясокурицы', 'картошка'],
-    priceCooking: 0,
-    price:  120, //['120 руб за 200 гр.', '220 руб за 400 гр.'],    
-    image: "https://voyagerestaurant.ru/wp-content/uploads/2020/07/soup_1.jpg",
-  },
-   { 
-    title:'Вареники с картошкой',
-    sostav: ['Тесто', 'картошка', 'лук'],
-    priceCooking: 0,
-    price: 100, //['100 руб за 120 гр.', '200 руб за 240 гр.'],   
-    veganfood: true,
-    image: "https://voyagerestaurant.ru/wp-content/uploads/2020/07/hot_3.jpg",
-   },
-   {
-    title:'Морс из брусники',
-    sostav: ['Брусника', 'морс', 'сахар'],
-    priceCooking: 0,
-    price: 100, //['100 руб за 120 гр.', '200 руб за 240 гр.'],   
-    veganfood: true,
-    image: "https://voyagerestaurant.ru/wp-content/uploads/2020/07/mors.jpg",
-  }
-  ];
+
   return (
     <div className="mx-auto grid grid-cols-2 gap-2 container md:grid-cols-3 xl:grid-cols-4">
       {menu.map((item) => (
