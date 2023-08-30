65d489b2d6f3a3591e618f9e60cc1614f9fb32a3
Maxinem10 <maxinem190790@gmail.com>
2023-08-31 00:59:49 +0800

    fix AsinkThunk

--- src/components/MenuFood/index.js
+++ src/components/MenuFood/index.js
@@ -1,18 +1,42 @@
+// // import MenuCard from "../../components/MenuCard";
+// import { useDispatch, useSelector } from "react-redux";
+// import { loadMenu } from "../../store/slices/menuSlice";
+// import { useEffect } from "react";
+// import LoadingError from "../../components/LoadingError";
+// import LoadingStart from "../../components/LoadingStart";
+// import MenuFood from "../../components/MenuFood/MenuCard";
 
-import menu from "../FakeData/menu";
-import MenuCard from "./MenuCard";
 
 
-const Menu = ({onItemBuy}) => {
+// const MenuPage = () => {
+//   const menu = useSelector((state) => state.menu);
+//   // const [category, setCategory] = useState()
 
-  return (
-    <div className="mx-auto grid grid-cols-2 gap-2 container md:grid-cols-3 xl:grid-cols-4">
-      {menu.map((item) => (
-        <MenuCard item={item}/>
-      ))}
-      
-    </div>
-  );
-};
+//   const dispatch = useDispatch();
 
-export default Menu;
+//   useEffect(() => {
+//     dispatch(loadMenu());
+//   }, []);
+
+//   return (
+//     <>
+//       {loadMenu === "fulfilled" && (
+//         <div className={"grid gap-4 grid-cols-2 md:grid-cols-3 xl:grid-cols-4"}>
+//           {menu.items.map((item) => (
+//             <MenuFood item={item} />
+//           ))}
+//         </div>
+//       )}
+
+//       {loadMenu === "pending" && (
+//         <LoadingStart message="Loading in progress" />
+//       )}
+
+//       {loadMenu === "rejected" && (
+//         <LoadingError message="Error loading data" />
+//       )}
+//     </>
+//   );
+// };
+
+// export default MenuPage;
